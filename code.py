7a6e65507dfe78aa501795e9c89315de1952d6a0
sundara-raghav <g.sundararaghav2005@gmail.com>
2024-10-12 19:20:19 +0530

    added

--- code.py
+++ code.py
@@ -3,7 +3,6 @@
 import csv
 import matplotlib.pyplot as plt
 
-# Initialize data containers
 years = []
 population_data = {
     'Tamil Nadu': [],
@@ -13,7 +12,6 @@ population_data = {
     'Telangana': []
 }
 
-# Read the CSV file
 with open(r'Python_mini_project\Population.csv', mode='r') as file:
     reader = csv.DictReader(file)
     for row in reader:
@@ -21,7 +19,6 @@ with open(r'Python_mini_project\Population.csv', mode='r') as file:
         for state in population_data.keys():
             population_data[state].append(float(row[state]))
 
-# Bar chart: Population of South Indian States in 2020
 states = list(population_data.keys())
 population_2020 = [population_data[state][years.index(2020)] for state in states]  # Data for 2020
 
@@ -31,14 +28,13 @@ plt.xlabel('State')
 plt.ylabel('Population (Millions)')
 plt.show()
 
-# Histogram: Distribution of populations in 2020
 plt.hist(population_2020, bins=5, edgecolor='black', color='skyblue')
 plt.title('Population Distribution in South Indian States (2020)')
 plt.xlabel('Population (Millions)')
 plt.ylabel('Frequency')
 plt.show()
 
-# Line graph: Population trends of South Indian States
+
 for state, populations in population_data.items():
     plt.plot(years, populations, label=state, marker='o')
 
@@ -49,7 +45,7 @@ plt.legend()
 plt.grid(True)
 plt.show()
 
-# Pie chart: Population distribution among South Indian States (2020)
+
 plt.pie(population_2020, labels=states, autopct='%1.1f%%', colors=['blue', 'green', 'red', 'orange', 'purple'])
 plt.title('Population Distribution in South Indian States (2020)')
 plt.show()
